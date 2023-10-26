b84adbb2b87d63dafbe52bcee59586e7adaa2ac4
aryanxarora <aryanxarora@gmail.com>
2023-10-26 14:55:42 -0400

    Invoice Building

--- src/components/InvoiceInfo.jsx
+++ src/components/InvoiceInfo.jsx
@@ -1,5 +1,10 @@
 function InvoiceInfo() {
-  return <div></div>;
+  return (
+    <div className="bg-slate-200 h-[90%] flex p-10">
+      <div className="basis-2/3 bg-white shadow-lg">sheet</div>
+      <div className="basis-1/3 ml-10">Info</div>
+    </div>
+  );
 }
 
 export default InvoiceInfo;
