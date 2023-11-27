b59d2cb488bbda00630e2abb52785a1cba169b9f
Jolson Cruz <56365930+jolsoncruz@users.noreply.github.com>
2023-11-27 16:22:53 -0500

    Update InvoiceInfo.jsx

--- src/components/InvoiceInfo.jsx
+++ src/components/InvoiceInfo.jsx
@@ -7,28 +7,63 @@ import logo from "../assets/logo.png";
 function InvoiceInfo() {
   const pdfRef = useRef();
 
+  // const downloadPDF = () => {
+  //   const input = pdfRef.current;
+  //   html2canvas(input).then((canvas) => {
+  //     const imgData = canvas.toDataURL('image/png');
+  //     const pdf = new jsPDF('p', 'mm', 'a4', true);
+  //     const pdfWidth = pdf.internal.pageSize.getWidth();
+  //     const pdfHeight = pdf.internal.pageSize.getHeight();
+  //     const imgWidth = canvas.width;
+  //     const imgHeight = canvas.height;
+  //     const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
+  //     const imgX = (pdfWidth - imgWidth * ratio) / 2;
+  //     const imgY = 30;
+  //     pdf.addImage(imgData, 'PNG', imgX, imgY, imgWidth * ratio, imgHeight * ratio);
+  //     pdf.save('invoice.pdf');
+  //   });
+  // }
+
+  // const downloadPDF = () => {
+  //   const input = pdfRef.current;
+  //   const doc = new jsPDF('p', 'pt', 'a4', true);
+
+  //   doc.html(input, {
+  //     callback: function(pdf){
+  //       var pageCount = doc.internal.getNumberOfPages();
+  //       pdf.deletePage(pageCount);
+  //       pdf.save("invoice.pdf");
+  //     },
+  //   })
+  // }
+
   const downloadPDF = () => {
-    const input = pdfRef.current;
-    html2canvas(input).then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
-      const pdf = new jsPDF('p', 'mm', 'a4', true);
-      const pdfWidth = pdf.internal.pageSize.getWidth();
-      const pdfHeight = pdf.internal.pageSize.getHeight();
-      const imgWidth = canvas.width;
-      const imgHeight = canvas.height;
-      const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
-      const imgX = (pdfWidth - imgWidth * ratio) / 2;
-      const imgY = 30;
-      pdf.addImage(imgData, 'PNG', imgX, imgY, imgWidth * ratio, imgHeight * ratio);
-      pdf.save('invoice.pdf');
+    var doc = new jsPDF({
+      compress: true,
+      putOnlyUsedFonts:true
+    });
+	
+    // Source HTMLElement or a string containing HTML.
+    var elementHTML = document.querySelector("#pdfContainer");
+
+    doc.html(elementHTML, {
+        callback: function(doc) {
+            // Save the PDF
+            doc.save('invoice.pdf');
+        },
+        margin: [10, 10, 10, 10],
+        autoPaging: 'text',
+        x: 0,
+        y: 0,
+        width: 190, //target width in the PDF document
+        windowWidth: 675 //window width in CSS pixels
     });
   }
-
   return (
     <div className="bg-slate-200 h-[90%] flex p-10">
 
-      <div id="pdfSection" className="basis-2/3 bg-white shadow-lg rounded-lg  overflow-scroll overflow-x-hidden">
-        <div id="pdfContainer" className="p-5 m-5" ref={pdfRef}>
+      <div id="pdfSection" className="p-5 basis-2/3 bg-white shadow-lg rounded-lg  overflow-scroll overflow-x-hidden">
+        <div id="pdfContainer" className="p-5" ref={pdfRef}>
           
           <div className="flex items-center justify-between mb-8">
             <div class="flex items-center">
@@ -90,7 +125,7 @@ function InvoiceInfo() {
 
           <div class="border-t-2 border-gray-300 pt-8 mb-8">
             <div class="text-gray-700 mb-2">Payment is due within 30 days. Late payments are subject to fees.</div>
-            <div class="text-gray-700 mb-2">Please make checks payable to Your Company Name and mail to:</div>
+            <div class="text-gray-700 mb-2">Please make checks payable to Vanier Mobility and mail to:</div>
             <div class="text-gray-700">123 Main St., Anytown, USA 12345</div>
           </div>
           
@@ -99,7 +134,7 @@ function InvoiceInfo() {
 
       <div id="infoSection" className="basis-1/3 ml-10">
         <h1>Info Section</h1>
-        <button onClick={downloadPDF}>Generate PDF</button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={downloadPDF}>Generate PDF</button>
       </div>
     </div>
   );
